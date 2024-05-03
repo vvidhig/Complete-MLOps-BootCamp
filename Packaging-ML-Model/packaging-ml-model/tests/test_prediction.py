9991b6abca28e7adecef5bd170c9695cab036373
nachiketh <support@manifoldailearning.in>
2024-05-03 17:44:19 +0530

    fix-pytest-code

--- Packaging-ML-Model/packaging-ml-model/tests/test_prediction.py
+++ Packaging-ML-Model/packaging-ml-model/tests/test_prediction.py
@@ -1,4 +1,13 @@
 import pytest
+
+from pathlib import Path
+import os
+import sys
+
+# Adding the below path to avoid module not found error
+PACKAGE_ROOT = Path(os.path.abspath(os.path.dirname(__file__))).parent
+sys.path.append(str(PACKAGE_ROOT))
+
 from prediction_model.config import config
 from prediction_model.processing.data_handling import load_dataset
 from prediction_model.predict import generate_predictions
